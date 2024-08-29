8aa126857024bc2a3fd091cd573290c4a2ab4006
Matías Deneken <matdknu@gmail.com>
2024-08-29 13:11:13 -0400

    new-vars

---
+++ code/latent_violence/new-var-model.R
@@ -0,0 +1,27 @@
+## Variables para el modelo.
+
+
+# Teoría de la identidad social. -----
+
+# a4: Escala de identidad	¿Cuánto se identifica con los (% PUEBLO ORIGINARIO)? 
+# a6: Escala de identidad	¿Cuánto se identifica con Chile?
+
+# Esta variable se puede crear en , si eres indígena, identificación con tu ingroup o outgrup.
+# Lo más aplica si eres no indígena, pero al revés las variables. 
+#
+
+# Justicia procidimental ------
+
+#`d5_1`: Carabineros tratan a las personas indígenas con respeto [Justicia procidimental]
+
+#`d5_2:` Carabineros tratan a las personas no indígenas con respeto [Justiciai procidimental]
+
+# Identificación con la causa. 
+
+# d6_1: identificación cuasa indígena
+
+# Teoría del contacto. ------
+
+
+#c7_1	Hoy en día, pensando en la gente con la que usted se relaciona cotidianamente ¿Cuántos de ellos son chilenos no-indígenas?
+#c12	Pensando en la gente con la que usted se relaciona cotidianamente ¿Cuántos de ellos diría usted que son (% PUEBLO ORIGINARIO)?
